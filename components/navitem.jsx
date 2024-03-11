d19e4fb9a4866edc4368b15dbdf4ca96d57c3eb9
superdev <newestStar1990@gmail.com>
2024-03-11 16:22:35 -0500

    added navitem

---
+++ components/navitem.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import * as Popover from "@radix-ui/react-popover";
+import { ChevronDown } from "react-feather";
+
+export default function ({ label, href = "#", dropdown = null }) {
+	if (dropdown) {
+		return (
+			<Popover.Root>
+				<Popover.Trigger>
+					<div className="py-2 px-3 text-lg font-bold flex items-center gap-1">
+						<div>{label}</div>
+						<ChevronDown />
+					</div>
+				</Popover.Trigger>
+				<Popover.Content>{dropdown}</Popover.Content>
+			</Popover.Root>
+		);
+	}
+	return (
+		<Link href={href} className="py-2 px-3 text-lg font-bold">
+			{label}
+		</Link>
+	);
+}
