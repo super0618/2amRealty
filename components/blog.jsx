07b9298f39fe310429a64f3a95596f1a86553abc
superdev <newestStar1990@gmail.com>
2024-03-16 14:46:05 -0400

    added blog list

--- components/blog.jsx
+++ components/blog.jsx
@@ -1,18 +1,24 @@
 import { IconCalendar } from "@tabler/icons-react";
 
-export default function () {
+export default function ({ thumb, title, content, username, date, avatar }) {
 	return (
 		<div className="border rounded-md bg-white shadow-sm">
 			<div className="p-2 mb-2">
-				<img src="/images/blogs/thumb1.jpg" alt="blog" />
+				<img src={thumb} alt="blog" />
 			</div>
-			<h2 className="font-bold px-2 ">Redfin Ranks the Most Competitive Neighborhoods of 2022</h2>
-			<p className="text-sm p-2 border-b">
-				Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Duis Mollis Et Sem Sed Sollicitudin. Donec Non Odio Neque. Aliquam Hendrerit
-				Sollicitudin Purus, Quis Rutrum Mi Accumsan Nec. Quisque Bibendum Orci Ac Nibh Facilisis, At Malesuada Orci Congue. Nullam Tempus Sollicitudin
-				Cursus.
-			</p>
-			<p className="p-2">123</p>
+			<h2 className="font-bold px-2 cursor-pointer hover:text-[#ff5a5f]">{title}</h2>
+			<p className="text-sm p-2 border-b">{content}</p>
+			<div className="grid grid-cols-3 p-4">
+				<div className="flex items-center gap-2">
+					<img src={avatar} alt="avatar" width="36px" className="rounded-full" />
+					<p>{username}</p>
+				</div>
+				<div className="flex items-center gap-1">
+					<IconCalendar size={24} style={{ padding: 1 }} />
+					<p>{date}</p>
+				</div>
+				<div className="text-[#ff5a5f] flex items-center justify-end">Read More</div>
+			</div>
 		</div>
 	);
 }
