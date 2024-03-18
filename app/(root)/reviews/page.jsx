2d5a0176c2dcc364d79d73a2f8ea145f93a22968
superdev <newestStar1990@gmail.com>
2024-03-18 09:28:03 -0400

    added navlinks

---
+++ app/(root)/reviews/page.jsx
@@ -0,0 +1,3 @@
+export default function () {
+	return <div className="bg-gray-50 pt-32 pb-12">My Reviews</div>;
+}
--- components/navitem.jsx
+++ components/navitem.jsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import * as Popover from "@radix-ui/react-popover";
 import { IconChevronDown } from "@tabler/icons-react";
 
-export default function ({ label, href = "#", dropdown = null }) {
+export default function ({ label, href = "#", dropdown = null, arrowdown = false }) {
 	if (dropdown) {
 		return (
 			<Popover.Root>
 				<Popover.Trigger>
-					<div className="py-2 px-3 text-lg font-bold flex items-center gap-[2px]">
+					<div className="py-2 px-3 text-lg flex items-center gap-[2px]">
 						<div>{label}</div>
-						<IconChevronDown />
+						{arrowdown && <IconChevronDown />}
 					</div>
 				</Popover.Trigger>
 				<Popover.Content>{dropdown}</Popover.Content>
@@ -17,7 +17,7 @@ export default function ({ label, href = "#", dropdown = null }) {
 		);
 	}
 	return (
-		<Link href={href} className="p-2 text-lg font-bold">
+		<Link href={href} className="p-2 text-lg flex items-center">
 			{label}
 		</Link>
 	);
